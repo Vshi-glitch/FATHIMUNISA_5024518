6692ff302f5c79cb2b7f68e214e75399df52f819
Vshi-glitch <fath21137.cs@rmkec.ac.in>
2024-08-07 17:08:20 +0530

    Delete Week 2 - PLSQL directory

--- Week 2 - PLSQL/Triggers/scenario1.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE TRIGGER UpdateCustomerLastModified
-BEFORE UPDATE ON customers
-FOR EACH ROW
-BEGIN
-   :NEW.lastmodified := SYSDATE;
-END UpdateCustomerLastModified;
-/
--- Week 2 - PLSQL/Triggers/scenario2.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TRIGGER LogTransaction
-AFTER INSERT ON transactions
-FOR EACH ROW
-BEGIN
-   INSERT INTO AuditLog (TransactionID, AccountID, TransactionDate, Amount, TransactionType)
-   VALUES (:NEW.TransactionID, :NEW.AccountID, :NEW.TransactionDate, :NEW.Amount, :NEW.TransactionType);
-END LogTransaction;
-/
